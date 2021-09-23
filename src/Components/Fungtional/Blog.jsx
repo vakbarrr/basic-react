d876115738223000b153fd1e16bd8e6efa9814ab
Gian Akbar <akbarigi20@gmail.com>
2021-09-24 06:14:38 +0700

    components

---
+++ src/Components/Fungtional/Blog.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Css/Blog.css'
+
+const Blog = (props) => {
+    return 
+    <div className="blog-wrap">
+        <img src="https://placeimg.com/640/480/tech" alt="header" />
+        <p>{props.tanggal}</p>
+        <p><b>{props.judul}</b></p>
+        <p>{props.summary}</p>
+    </div>
+}
+
+export default Blog;
---
+++ src/Components/Fungtional/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Blog from './Blog';
+
+const Home = () => {
+return <div>
+   
+   <Blog judul="test blog" tanggal="2021-02-02" summary="ini adalah blog"/>
+</div>
+}
+
+export default Home;
